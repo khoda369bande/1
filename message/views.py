810c6549ac0fe9e3a65f55df09efdbe0d7a6b4da
Khode._.Bande <khoda369bande@gmail.com>
2025-03-16 11:38:03 +0330

    message

--- message/views.py
+++ message/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Message
 
 #def messageView(request):
 #    return render(request, 'home.html')
 
-class MessageView(TemplateView):
+class MessageView(ListView):
+    model = Message
     template_name = 'home.html'
 
